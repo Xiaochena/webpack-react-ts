b89062e92e5745d5be67a6380b218088619591c4
小陈啊！！！ <chen.lingang@qq.com>
2022-07-03 20:45:10 +0800

    CleanWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
@@ -11,6 +12,7 @@ module.exports = {
   devServer: { static: "./dist" },
   devtool: "inline-source-map",
   plugins: [
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       title: "webpack-ts-rc",
     }),
