b836e1fb62ac1cb876076c89a905aa40a41ce159
小陈啊！！！ <chen.lingang@qq.com>
2022-07-03 22:06:17 +0800

    [feat] jsx

--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,26 @@ module.exports = {
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       title: "webpack-ts-rc",
+      template: "./public/index.html",
+      inject: "body",
     }),
   ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+            plugins: [
+              "@babel/plugin-transform-runtime",
+              "@babel/plugin-proposal-class-properties",
+            ],
+          },
+        },
+      },
+    ],
+  },
 };
