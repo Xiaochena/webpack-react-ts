ce153b44570c2b71f2756d8d1dd543f4d98a6207
小陈啊！！！ <chen.lingang@qq.com>
2022-07-06 23:34:10 +0800

    css

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import React from "react";
+import "./app.css";
 
 const App = () => {
-  return <h1>hello react</h1>;
+  return <h1 className="app">hello react</h1>;
 };
 
 export default App;
--- webpack.config.ts
+++ webpack.config.ts
@@ -22,6 +22,10 @@ const config: webpack.Configuration = {
   module: {
     rules: [
       {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
         test: /\.(js|jsx|tsx)$/,
         exclude: /(node_modules|bower_components)/,
         use: {
